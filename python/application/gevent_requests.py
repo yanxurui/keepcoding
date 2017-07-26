d744e814d45ec6145caa4d9888884fe05be1b16c
yanxurui <617080352@qq.com>
2017-07-26 15:04:55 +0800

    python/application: use imap_unordered

--- python/application/gevent_requests.py
+++ python/application/gevent_requests.py
@@ -7,51 +7,51 @@ from gevent import monkey
 
 monkey.patch_all()
 
-repo_star = {}
 def get(repo):
+    print(repo)
     r = requests.get('https://api.github.com/repos/%s?access_token=4a5f58347e8fcbf77ab50c515c2e61c1f0e9b8bc' % repo)
     if r.status_code != 200:
         print('bad status code %s:%d' % (repo, r.status_code))
-        return
+        return repo, 0
     j = r.json()
-    try:
-        repo_star[repo] = j['stargazers_count']
-    except KeyError as e:
-        print(j)
-        raise
+    return repo, j['stargazers_count']
 
 def main():
-    count = 0
     r=requests.get('http://flask.pocoo.org/extensions/')
     body = r.text
     repos = map(lambda x: x.rstrip('/'), re.findall('http://github.com/(.*?)"', body))
-    pool = Pool(5)
-    pool.map(get, repos)
-    global repo_star
+    pool = Pool(10)
+    repo_star = {}
+    for repo, star in pool.imap_unordered(get, repos):
+        repo_star[repo] = star
     repo_star = sorted(repo_star.items(), key=lambda x: x[1])
     for repo, star in repo_star:
-        print(repo, star)
+        print('%s : %d' % (repo, star))
 
 if __name__ == '__main__':
     main()
 
 
-
 """
-top 15
-(u'mitsuhiko/flask-oauth', 480)
-(u'lingthio/flask-user', 509)                            
-(u'thadeusb/flask-cache', 520)                           
-(u'techniq/flask-script', 574)                           
-(u'coleifer/flask-peewee', 575)                          
-(u'mgood/flask-debugtoolbar', 577)                       
-(u'ajford/flask-wtf', 632)  
-(u'miguelgrinberg/flask-migrate', 711)                   
-(u'jfinkels/flask-restless', 787)                        
-(u'mattupstate/flask-security', 928)                     
-(u'johnwheeler/flask-ask', 1071)                         
-(u'maxcountryman/flask-login', 1462)                     
-(u'mitsuhiko/flask-sqlalchemy', 1697)                    
-(u'flask-admin/flask-admin', 2362)                       
-(u'flask-restful/flask-restful', 2876)
+top 20 on 2017-07-26
+mattupstate/flask-mail : 324
+dcrosta/flask-pymongo : 337
+mattupstate/flask-principal : 351
+apiguy/flask-classy : 469
+SimonSapin/Frozen-Flask : 473
+mitsuhiko/flask-oauth : 480
+lingthio/flask-user : 509
+thadeusb/flask-cache : 520
+techniq/flask-script : 574
+coleifer/flask-peewee : 575
+mgood/flask-debugtoolbar : 577
+ajford/flask-wtf : 632
+miguelgrinberg/flask-migrate : 711
+jfinkels/flask-restless : 787
+mattupstate/flask-security : 928
+johnwheeler/flask-ask : 1071
+maxcountryman/flask-login : 1462
+mitsuhiko/flask-sqlalchemy : 1697
+flask-admin/flask-admin : 2362
+flask-restful/flask-restful : 2876
 """
