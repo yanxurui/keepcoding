49bf8bc633f337da50479fea2a9f70e99c122667
Xurui Yan <xuryan@microsoft.com>
2024-07-12 15:52:47 -0700

    Minor update

--- c#/GrpcBenchmark/GrpcGreeterTest/Program.cs
+++ c#/GrpcBenchmark/GrpcGreeterTest/Program.cs
@@ -4,38 +4,73 @@ using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Grpc.Net.Client;
 using Greet;
+using System.Threading.Channels;
 
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:5001");
 var client = new Greeter.GreeterClient(channel);
 DownloadRequest downloadRequest = new DownloadRequest { RequestSize = 10 };
 
-Stopwatch stopwatch = new Stopwatch();
-stopwatch.Start();
-
-//Task<Empty> sleep5 = client.SleepAsync(new SleepRequest { Seconds = 0 }).ResponseAsync;
-//Task<Empty> sleep1 = client.SleepAsync(new SleepRequest { Seconds = 0 }).ResponseAsync;
-//Task task1 = Sleep(5);
-//Task task2 = Sleep(1);
-//Task task1 = DownloadStream();
-//Task task2 = DownloadStream();
-//await PrintThread();
-List<Task> tasks = new List<Task>();
-for (int i = 0; i < 10; i++)
+/// <summary>
+/// Test blocking operations in the server side will create new threads
+/// </summary>
+async Task Sleep()
 {
-    tasks.Add(DownloadStream());
+    Task<Empty> sleep5 = client.SleepAsync(new SleepRequest { Seconds = 5 }).ResponseAsync;
+    Task<Empty> sleep1 = client.SleepAsync(new SleepRequest { Seconds = 0 }).ResponseAsync;
+    Task task1 = Sleep(5);
+    Task task2 = Sleep(1);
+    await PrintThread();
+
+    async Task Sleep(int seconds)
+    {
+        await client.SleepAsync(new SleepRequest { Seconds = (uint)seconds });
+        await PrintThread();
+    }
 }
-await Task.WhenAll(tasks);
 
-stopwatch.Stop();
-Console.WriteLine("Elapsed time {0} seconds", stopwatch.Elapsed.TotalSeconds);
-
-async Task Sleep(int seconds)
+/// <summary>
+/// Test if concurrent requests in the client can share the same thread
+/// </summary>
+async Task ConcurrentDownload()
 {
-    await client.SleepAsync(new SleepRequest { Seconds = (uint)seconds });
-    int threadId = Thread.CurrentThread.ManagedThreadId;
-    Console.WriteLine($"Thread id: {threadId}");
-    //await PrintThread();
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    List<Task> tasks = new List<Task>();
+    for (int i = 0; i < 10; i++)
+    {
+        tasks.Add(DownloadStream());
+    }
+    await Task.WhenAll(tasks);
+
+    stopwatch.Stop();
+    Console.WriteLine("Elapsed time {0} seconds", stopwatch.Elapsed.TotalSeconds);
+}
+
+
+/// <summary>
+/// Verify that EnableKernelResponseBuffering can help to improve throughput for large responses.
+/// 2 vs 14 seconds
+/// </summary>
+async Task LargeResponse()
+{
+    // http.sys
+    using var channel = GrpcChannel.ForAddress(
+        "https://127.0.0.1:5002",
+        new GrpcChannelOptions
+        {
+            MaxReceiveMessageSize = 200 * 1024 * 1024
+        });
+
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    var resp = await client.DownloadAsync(new DownloadRequest { RequestSize = 200_000_000 });
+    File.WriteAllBytes(@".\out", resp.Body.ToByteArray());
+
+    stopwatch.Stop();
+    Console.WriteLine("Elapsed time {0} seconds", stopwatch.Elapsed.TotalSeconds);
 }
 
 async Task DownloadStream()
@@ -52,11 +87,9 @@ async Task DownloadStream()
     while (await stream.ResponseStream.MoveNext())
     {
         DownloadReply reply = stream.ResponseStream.Current;
-        // Process each reply here
         await PrintThread();
     }
 }
-
 async Task PrintThread()
 {
     int threadId = Thread.CurrentThread.ManagedThreadId;
--- c#/GrpcBenchmark/Readme.md
+++ c#/GrpcBenchmark/Readme.md
@@ -4,10 +4,38 @@ This is a benchmark for Grpc.AspNetCore.
 Start Kestrel by default. Alternatively, you can start Http.Sys which requires special setup for the cert.
 
 ## GrpcGreeterClient
-A wrk-like benchmark tool. Basically, you can specify how many concurrent connections to run and the payload size the server responds.
+A wrk-like benchmark tool. You can the payload size the server responds and how many concurrent connections to run, etc.
 
 ```
-dotnet C:\code\keepcoding\c#\GrpcBenchmark\GrpcGreeterClient\bin\Release\net8.0\GrpcGreeterClient.dll -U https://localhost:5001 -R 1 --maxSize 10
+> dotnet C:\code\keepcoding\c#\GrpcBenchmark\GrpcGreeterClient\bin\Release\net8.0\GrpcGreeterClient.dll --help
+
+GrpcGreeterClient 1.0.0+67cf74e652802bd71a98dd0b1d1a4304ff183593
+Copyright (C) 2024 GrpcGreeterClient
+
+  -U, --url           (Default: https://localhost:5000) Url to do load test against
+
+  -R, --run           (Default: 3) Number of runs
+
+  -D, --duration      (Default: 20) Duration (seconds) to run the test
+
+  -T, --thread        (Default: 5) Count of threads
+
+  -C, --connection    (Default: 80) Concurrent connections per thread.
+
+  -S, --streaming     (Default: false) whether to call the streaming rpc.
+
+  --maxSize           (Default: 1000000)
+
+  --minSize           (Default: 10)
+
+  --help              Display this help screen.
+
+  --version           Display version information.
+```
+
+```
+> dotnet C:\code\keepcoding\c#\GrpcBenchmark\GrpcGreeterClient\bin\Release\net8.0\GrpcGreeterClient.dll -U https://localhost:5001 -R 1 --maxSize 10
+
 ===================================
 Round 1/1
 ===================================
